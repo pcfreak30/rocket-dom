e3ebe380d219b3dbe501062b95573268dbc7b947
Derrick Hammer <derrick@derrickhammer.com>
2019-06-30 01:13:24 -0400

    *Remove extra brace

--- src/DOMTagNameCollection.php
+++ src/DOMTagNameCollection.php
@@ -5,6 +5,6 @@ namespace pcfreak30\RocketDOM;
 
 class DOMTagNameCollection extends DOMXPathCollection {
 	public function __construct( DOMDocument $document, $tag_name ) {
-		parent::__construct( $document, "//{$tag_name}}", new \DOMXPath( $document ) );
+		parent::__construct( $document, "//{$tag_name}", new \DOMXPath( $document ) );
 	}
 }
