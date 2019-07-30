0a023c8b2f311d311bad8f76e04ecf9ce1ee53ec
Derrick Hammer <derrick@derrickhammer.com>
2019-07-30 00:34:58 -0400

    *refactor DOMDocumentTest class to use a doc property in a _before and cleanup in _after

--- tests/unit/DOMDocumentTest.php
+++ tests/unit/DOMDocumentTest.php
@@ -4,36 +4,44 @@ use Codeception\Test\Unit;
 use pcfreak30\RocketDOM\DOMDocument;
 
 class DOMDocumentTest extends Unit {
+	/**
+	 * @var DOMDocument
+	 */
+	private $doc;
 
 	public function test_get_nodes_by_type() {
 
-		$doc = new DOMDocument();
-		$this->assertTrue( $doc->loadHTML( DOM_TEST_HTML ) );
+		$this->assertTrue( $this->doc->loadHTML( DOM_TEST_HTML ) );
 
-		$collection = $doc->get_nodes_by_type( 'script' );
+		$collection = $this->doc->get_nodes_by_type( 'script' );
 		$this->assertInstanceOf( '\pcfreak30\RocketDOM\DOMTagNameCollection', $collection );
 		$this->assertEquals( 7, $collection->count() );
 	}
 
 	public function test_loadHTML() {
-		$doc = new DOMDocument();
-		$this->assertTrue( $doc->loadHTML( DOM_TEST_HTML ) );
+		$this->assertTrue( $this->doc->loadHTML( DOM_TEST_HTML ) );
 	}
 
 	public function test_saveHTML() {
-		$doc = new DOMDocument();
-		$doc->loadHTML( DOM_TEST_HTML );
+		$this->doc->loadHTML( DOM_TEST_HTML );
 
-		$html = $doc->saveHTML();
+		$html = $this->doc->saveHTML();
 
 		$this->assertEquals( 8, substr_count( $html, '<script' ) );
 	}
 
 	public function test_get_nodes_by_xpath() {
-		$doc = new DOMDocument();
-		$doc->loadHTML( DOM_TEST_HTML );
+		$this->doc->loadHTML( DOM_TEST_HTML );
 
-		$collection = $doc->get_nodes_by_xpath( '//body' );
+		$collection = $this->doc->get_nodes_by_xpath( '//body' );
 		$this->assertEquals( 1, $collection->count() );
+	}
+
+	protected function _before() {
+		$this->doc = new DOMDocument();
+	}
+
+	protected function _after() {
+		$this->doc = null;
 	}
 }
